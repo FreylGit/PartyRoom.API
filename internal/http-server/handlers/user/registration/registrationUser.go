a2b236ad8437a4e164d7eba0391d538f63606fd0
ANDREY <41635263+FreylGit@users.noreply.github.com>
2023-12-25 00:23:19 +0300

    Создал проект, начал настройку авторизации

---
+++ internal/http-server/handlers/user/registration/registrationUser.go
@@ -0,0 +1,43 @@
+package registrationUser
+
+import (
+	"PartyRoom.API/internal/domain"
+	"PartyRoom.API/internal/lib/api/response"
+	"github.com/go-chi/render"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
+type Request struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	Name     string `json:"name" validate:"required"`
+	Phone    string `json:"phone" validate:"required"`
+}
+type UserRepository interface {
+	CreateUser(user domain.User) error
+}
+
+func New(repository UserRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req Request
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			render.JSON(w, r, response.Error("invalid request"))
+			return
+		}
+		passwordHash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+		newUser := domain.User{
+			Email:        req.Email,
+			PasswordHash: string(passwordHash),
+			Name:         req.Name,
+			Phone:        req.Phone,
+		}
+		err = repository.CreateUser(newUser)
+		if err != nil {
+			render.JSON(w, r, response.Error(err.Error()))
+			return
+		}
+		render.JSON(w, r, response.Ok())
+	}
+}
